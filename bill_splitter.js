1bc5938c762cae3d2e973bc5cc4c84dc0f1e8b80
Rashi Ratan <rashiiratan@gmail.com>
2024-09-07 18:00:30 -0400

    Initial logic for calculating tip using ternary operator

---
+++ bill_splitter.js
@@ -0,0 +1,8 @@
+//Task 1: Calculate the Tip
+let x = 500; //let bill amount be x
+if (x >= 50 && x <= 300) {//using the && operator to identify bill values within required range
+    var t = x*0.15;// let tip be t
+
+} else {
+    var t = x*0.2;
+}
